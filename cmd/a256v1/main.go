5056f14f1074ae111855ecdb5d8894450226bc52
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2022-12-21 20:48:19 -0300

    Diffie-Hellman and Encryption functions

--- cmd/a256v1/main.go
+++ cmd/a256v1/main.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2023, Pedro Albanese. All rights reserved.
+// Use of this source code is governed by a ISC license that
+// can be found in the LICENSE file.
 package main
 
 import (
